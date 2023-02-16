49f23d2dc43878926fc54cfade095f88d6a6b72c
dekersab3u <dekersab3u@etu.univ-lorraine.fr>
2023-02-16 10:22:20 +0100

    add models for categorie

---
+++ src/models/categorie.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace ccd\models;
+use \Illuminate\Database\Eloquent as Eloq;
+
+class Categorie extends Eloq\Model
+{
+    protected $table = 'categorie';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    
+}
