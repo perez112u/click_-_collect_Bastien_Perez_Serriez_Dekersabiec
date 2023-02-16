0518649c5cd0c93cf638f7e42da0711ed68b6fbf
SERRIER Mathéo <matheo.serrier4@etu.univ-lorraine.fr>
2023-02-16 14:30:17 +0100

    Ajoute Catalogue.php et CatalogueRenderer.php

---
+++ src/models/Catalogue.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ccd\models;
+
+class Catalogue
+{
+
+}
---
+++ src/render/CatalogueRenderer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ccd\render;
+
+class CatalogueRenderer implements Renderer
+{
+
+    public function render(int $selector): string
+    {
+        // TODO: Implement render() method.
+    }
+}
